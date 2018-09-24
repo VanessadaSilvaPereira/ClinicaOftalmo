394c7affe43695d91629d2e0d45d149efce8b741
Vanessa da Silva Pereira <vanessa.pereira787@gmail.com>
2018-09-24 09:50:27 -0300

    Commit Inicial

---
+++ app/src/main/java/com/inf3m171/vanessa/oftalmo/OftalmoActivity.java
@@ -0,0 +1,13 @@
+package com.inf3m171.vanessa.oftalmo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OftalmoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oftalmo);
+    }
+}
