64509de95f5808ceb589e9b078be9ebba7c8633e
Vanessa da Silva Pereira <vanessa.pereira787@gmail.com>
2018-09-24 11:49:04 -0300

    Montagem das telas

--- app/src/main/java/com/inf3m171/vanessa/oftalmo/LoginActivity.java
+++ app/src/main/java/com/inf3m171/vanessa/oftalmo/LoginActivity.java
@@ -3,7 +3,7 @@ package com.inf3m171.vanessa.oftalmo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class OftalmoActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/inf3m171/vanessa/oftalmo/paciente_ou_medicoActivity.java
@@ -0,0 +1,13 @@
+package com.inf3m171.vanessa.oftalmo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class paciente_ou_medicoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paciente_ou_medico);
+    }
+}
